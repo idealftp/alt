d69738434ff4144c2bf7e17f39f8b03f1b790af6
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2020-02-14 00:59:59 +0300

    test image get

--- flastest.py
+++ flastest.py
@@ -1,9 +1,14 @@
 #-*- coding: utf-8 -*-
 
 import os
+from keras.models import  Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing.image import load_img, img_to_array
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from flask_pymongo import PyMongo
-from werkzeug.utils import secure_filename
+# from werkzeug.utils import secure_filename
 
 # UPLOAD_dossier = 'uploads/'
 #dossier destination des téléchargements
@@ -13,6 +18,18 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'svg'])
 app = Flask(__name__)
 app.config['MONGO_URI']= 'mongodb://192.168.150.110:27017/firstDb'
 mongo = PyMongo(app)
+
+
+def whatis(file):
+    file = str(file)
+    model= VGG16()
+    img= load_img(file, target_size=(224,224)) #charger l'image
+    img=img_to_array(img) # convertir le tableau en numpy
+    img=img.reshape((1, img.shape[0], img.shape[1], img.shape[2])) # creer la collection d'image
+    img=preprocess_input(img) # pretraiter l'image
+    y=model.predict(img)  # predire la classe de l'image
+    return('top 3:', decode_predictions(y, top=3)[0])
+
 
 def fichier_autorise(filename):
     return '.' in filename and \
@@ -27,7 +44,7 @@ def telechargeFichier():
             print('ito e >>', file.filename)
             mongo.save_file(file.filename, file)
             mongo.db['files'].insert({'username': 'root', 'image': file.filename})
-            return  'ok'
+            return  whatis(mongo.send_file(file.filename))
 
     return '''
         <!doctype html>
